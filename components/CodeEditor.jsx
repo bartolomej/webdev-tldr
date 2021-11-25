f230caa252dc92288e6ab694c7a382c17b63c664
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-25 20:11:41 +0100

    feat: extended iframe - support js execution with no preview - add iframe logs forwarding to parent

--- components/CodeEditor.jsx
+++ components/CodeEditor.jsx
@@ -4,14 +4,17 @@ import styled from 'styled-components'
 import { AiOutlineFullscreen, AiOutlineFullscreenExit, AiFillStepForward } from "react-icons/ai";
 import { IoPlayOutline } from "react-icons/io5";
 
-import "ace-builds/src-noconflict/mode-jsx";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-css"
+import "ace-builds/src-noconflict/mode-html"
+
 import "ace-builds/src-min-noconflict/ext-searchbox";
 import "ace-builds/src-min-noconflict/ext-language_tools";
+
 import "ace-builds/src-noconflict/theme-xcode"
-import "ace-builds/src-noconflict/mode-html"
 import "ace-builds/src-noconflict/snippets/html"
-import "ace-builds/src-noconflict/mode-css"
 import "ace-builds/src-noconflict/snippets/css"
+import "ace-builds/src-noconflict/snippets/javascript"
 
 
 function CodeEditor ({
@@ -24,7 +27,8 @@ function CodeEditor ({
   height = '500px',
   width = '100%',
   fullscreen = false,
-  autorun = false
+  autorun = false,
+  websitePreview = true
 }) {
   const [value, setValue] = useState(code);
   const [isFocused, setFocused] = useState(false);
@@ -40,9 +44,34 @@ function CodeEditor ({
   }, [fullscreen])
 
   useEffect(() => {
+    listenIframeEvents();
     window.addEventListener("keydown", onKeyPress);
     return () => window.removeEventListener("keydown", onKeyPress)
   }, [])
+
+  function listenIframeEvents() {
+    window.addEventListener('message', function(response) {
+      // Make sure message is from our iframe, extensions like React dev tools might use the same technique and mess up our logs
+      if (response.data && response.data.source === 'iframe') {
+        const payload = JSON.parse(response.data.message);
+        switch (payload.type) {
+          case "log": return handleLogMessage(payload);
+        }
+      }
+    });
+  }
+
+  function handleLogMessage(payload) {
+    switch (payload.logLevel) {
+      case "warn":
+      case "error":
+      case "info":
+      case "log": {
+        // TODO: handle logs
+        console.log("log: ", payload.arguments)
+      }
+    }
+  }
 
   function onKeyPress (e) {
     if (e.key === "Escape") {
@@ -53,11 +82,64 @@ function CodeEditor ({
     }
   }
 
-  function updateIframeContent() {
+  function formatContent() {
+    switch (lang) {
+      case "javascript": return `<script>${value}</script>`
+      default: return value;
+    }
+  }
+
+  function internalScripts() {
+    return `
+      <script>
+        // Save the current console log function in case we need it.
+        const overwrites = Object.create({
+          log: console.log,
+          error: console.error,
+          warn: console.warn,
+          info: console.info,
+        });
+        
+        function emitMessage(data) {
+          window.parent.postMessage(
+            {
+              source: 'iframe',
+              message: JSON.stringify(data),
+            },
+            '*'
+          );
+        }
+        
+        function emitLog(logLevel) {
+          return function (...rest) {
+            // window.parent is the parent frame that made this window
+            emitMessage({
+              type: "log",
+              logLevel,
+              arguments: rest
+            })  
+            // Finally applying the console statements to saved instance earlier
+            overwrites[logLevel].apply(console, arguments);
+          };
+        }
+        
+        // Override the console
+        console.log = emitLog("log");
+        console.error = emitLog("error");
+        console.warn = emitLog("warn");
+        console.info = emitLog("info");
+      </script>
+    `
+  }
+
+  function updateIframeContent () {
     const iframeDoc = iframeRef.current.contentDocument;
+    const iframeWindow = iframeRef.current.contentWindow;
     iframeDoc.open();
-    iframeDoc.write(value);
+    iframeDoc.write(internalScripts());
+    iframeDoc.write(formatContent());
     iframeDoc.close();
+    iframeWindow.location.reload(); // clean JS context (clear declared variables,..)
   }
 
   useEffect(() => {
@@ -122,9 +204,13 @@ function CodeEditor ({
             }}
           />
         </EditorSide>
-        <PreviewSide>
-          <Iframe ref={iframeRef}/>
-        </PreviewSide>
+        {websitePreview ? (
+          <PreviewSide>
+            <Iframe ref={iframeRef}/>
+          </PreviewSide>
+        ) : (
+          <IframeHidden ref={iframeRef} />
+        )}
       </Container>
     </OuterContainer>
   )
@@ -164,7 +250,7 @@ const ControlButton = styled.button`
   padding: 3px;
   border: 2px solid ${({ theme }) => theme.colors.light};
   border-radius: ${({ theme }) => theme.constants.smBorderRadius};
-  margin-left: ${({theme}) => theme.gutter.sm};
+  margin-left: ${({ theme }) => theme.gutter.sm};
 `;
 
 const EditorSide = styled.div`
@@ -184,5 +270,9 @@ const Iframe = styled.iframe`
   border: none;
   background: white;
 `
+
+const IframeHidden = styled.iframe`
+  display: none;
+`;
 
 export default CodeEditor
